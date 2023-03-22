715b5ab1620353ef14eab8bfd28757698d4b57d6
MathibelaD <dineomathibela161@gmail.com>
2023-03-23 01:04:09 +0200

    check credit function

--- laezonia-university/src/Components/HomePage.tsx
+++ laezonia-university/src/Components/HomePage.tsx
@@ -4,6 +4,8 @@ export const HomePage = () => {
 
     return (<>
             <p> the averageScore is </p>
-            {criteriaService.calculateAverageScore(20,82,93,81)}        
+            {criteriaService.calculateAverageScore(20,82,93,81)}
+            {criteriaService.checkCredit(20,49,75,30)
+            }        
             </>)
 }
--- laezonia-university/src/Components/winnersService.tsx
+++ laezonia-university/src/Components/winnersService.tsx
@@ -2,7 +2,14 @@ import { IAcademicScores } from "./ParticipantsDto";
 
 
 export class criteriaService {
-    static calculateAverageScore(Math: any,Biology: any,Physics: any,Research: any){
+    static calculateAverageScore(Math: number,Biology: number,Physics: number,Research: number){
         return (Math + Biology + Physics + Research) / 400 * 100
+    }
+    static checkCredit(Math: number,Biology: number,Physics: number,Research: number) {
+        const score = this.calculateAverageScore(Math,Biology,Physics,Research)
+        if(score > 65 || Physics >= 75){
+            return 1
+        } else 
+        {return 0}
     }
 }
