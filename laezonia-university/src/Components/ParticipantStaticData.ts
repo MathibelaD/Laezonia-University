463ea52930c24702adddf3a6123ee1f6de299387
MathibelaD <dineomathibela161@gmail.com>
2023-03-22 13:13:14 +0200

    participants data/info

---
+++ laezonia-university/src/Components/ParticipantStaticData.ts
@@ -0,0 +1,93 @@
+import {ParticipantsDto} from "./ParticipantsDto"
+
+export const ParticipantsStaticData: ParticipantsDto[] = [{
+    participantId: "SB1009",
+    acardemicScore: {
+        Math: 89,
+        Physics: 68,
+        Biology: 78,
+        Reseacher: 56
+    },
+    maratonResults: 21.4
+},
+{
+    participantId: "SB3001",
+    acardemicScore: {
+        Math: 78,
+        Physics: 90,
+        Biology: 88,
+        Reseacher: 66
+    },
+    maratonResults: 30.6
+},
+{
+    participantId: "SB5990",
+    acardemicScore: {
+        Math: 88,
+        Physics: 90,
+        Biology: 65,
+        Reseacher: 70
+    },
+    maratonResults: 25.8
+},
+{
+    participantId: "SB9883",
+    acardemicScore: {
+        Math: 90,
+        Physics: 68,
+        Biology: 88,
+        Reseacher: 93
+    },
+    maratonResults: 34.4
+},
+{
+    participantId: "SB6743",
+    acardemicScore: {
+        Math: 93,
+        Physics: 78,
+        Biology: 90,
+        Reseacher: 89
+    },
+    maratonResults: 12.8
+},
+{
+    participantId: "SB9098",
+    acardemicScore: {
+        Math: 85,
+        Physics: 88,
+        Biology: 78,
+        Reseacher: 67
+    },
+    maratonResults: 20.4
+},
+{
+    participantId: "SB6743",
+    acardemicScore: {
+        Math: 88,
+        Physics: 69,
+        Biology: 90,
+        Reseacher: 67
+    },
+    maratonResults: 36.2
+},
+{
+    participantId: "SB2533",
+    acardemicScore: {
+        Math: 60,
+        Physics: 85,
+        Biology: 89,
+        Reseacher: 78
+    },
+    maratonResults: 37.7
+},
+{
+    participantId: "SB5463",
+    acardemicScore: {
+        Math: 91,
+        Physics: 86,
+        Biology: 72,
+        Reseacher: 82
+    },
+    maratonResults: 27.9
+}
+]
